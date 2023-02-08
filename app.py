ff182c2019c527534c8aeaa3e390b3b4d5bece0c
AlvaroR123 <124704463+AlvaroR123@users.noreply.github.com>
2023-02-08 15:01:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+st.image("p.png")
 ##Al momento que salgan las nuevas bases oficom se deben actualizar las fechas, el AÑO debe ir en formato año_anterior - año_actual, 
 # y MES enero-mes_actual
 MES = "Enero-Diciembre"
